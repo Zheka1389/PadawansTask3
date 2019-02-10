bf7cd5789a9573402a986d21d1c08a89dfcf2b0d
Prystauka Yauheni <34281826+Zheka1389@users.noreply.github.com>
2019-02-10 23:08:25 +0300

    1

--- IntegerExtensions.cs
+++ IntegerExtensions.cs
@@ -7,7 +7,20 @@ namespace PadawansTask3
         public static int Gcd(int a, int b)
         {
             // put your code here
-            throw new NotImplementedException();
+            // throw new NotImplementedException();
+            checked
+            {
+                if (a == 0 || b == 0)
+            {
+                throw new ArgumentException();
+            }
+            else
+            {
+                while (b != 0)
+                    b = a % (a = b);
+                return a;
+            }
+            }
         }
     }
 }
